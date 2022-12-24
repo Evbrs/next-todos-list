09f551edac029db31a2bed0ad359a26efcfbc34f
Evan Barras <ev.brs@outlook.com>
2022-12-24 23:52:07 +0100

    add context and add components into index

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
 import '../styles/globals.css'
+import Context from '../INITIAL_STATES/context'
+import INITIAL_TODOS from '../INITIAL_STATES/todos'
+import INITIAL_ISCHECKED from '../INITIAL_STATES/isChecked'
+import INITIAL_INDEXOFTODOS from '../INITIAL_STATES/indexOfTodos'
+import INITIAL_SHOWMODAL from '../INITIAL_STATES/showModal'
+
+import { useState } from 'react'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [index, setIndex] = useState(INITIAL_INDEXOFTODOS)
+  const [todos, setTodos] = useState(INITIAL_TODOS)
+  const [isChecked, setIsChecked] = useState(INITIAL_ISCHECKED)
+  const [showAddTasksModal, setShowAddTasksModal] = useState(INITIAL_SHOWMODAL)
+  const [showAddTodosModal, setShowAddTodosModal] = useState(INITIAL_SHOWMODAL)
+  const [showEditModal, setShowEditModal] = useState(INITIAL_SHOWMODAL)
+
+  return (
+    <Context.Provider value={{ todos, setTodos, isChecked, setIsChecked, index, setIndex, showAddTasksModal, setShowAddTasksModal, showAddTodosModal, setShowAddTodosModal }}>
+      <Component {...pageProps} />
+    </Context.Provider>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,19 @@
 import ActionsHeader from '../components/business/ActionsHeader'
+import AddTasksModal from '../components/business/AddTasksModal'
+import AddTodoModal from '../components/business/AddTodoModal'
 import List from '../components/business/List'
 import TodosHeaders from '../components/business/TodosHeaders'
-import Button from '../components/ui/Button'
 
 export default function Home() {
   return (
-    <div className='pt-1'>
+    <div className='h-screen overflow-y-hidden'>
       <TodosHeaders />
       <ActionsHeader />
-      <div className='flex-1 overflow-y-scroll'>
-
+      <div className='h-screen overflow-y-scroll'>
         <List />
       </div>
+      <AddTasksModal />
+      <AddTodoModal />
     </div>
   )
 }
